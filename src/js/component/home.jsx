6bbb59b3b5e943336a3b00e5b44988253c8a61e4
rebecabergottini <117315880+rebecabergottini@users.noreply.github.com>
2023-04-18 09:50:36 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,24 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
+	console.log(props)
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+<div className="bigCounter">
+      <div className="calendar">
+	  <FontAwesomeIcon icon={faClock} />
+	  </div>			
+		<div className="six">{props.hora2}</div>
+		<div className="five">{props.hora1}</div>
+      <div className="four">{props.minuto2}</div>
+      <div className="three">{props.minuto1}</div>
+      <div className="two">{props.segundo2}</div>
+      <div className="one">{props.segundo1}</div>
 		</div>
 	);
 };
 
 export default Home;
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,50 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
-
-//import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let digitSec1 = 0;
+let digitSec2 = 0;
+let digitMin1 = 0;
+let digitMin2 = 0;
+let digitHour1 = 0;
+let digitHour2 = 0;
+
+
+setInterval(function () {
+  digitSec1++;
+  if (digitSec1 === 10) {
+    digitSec1 = 0;
+    digitSec2++;
+  }
+  if (digitSec2 === 6) {
+    digitSec2 = 0;
+    digitMin1++;
+  }
+  if (digitMin1 === 10) {
+    digitMin1 = 0;
+    digitMin2++;
+  }
+  if (digitMin2 === 6) {
+    digitMin2 = 0;
+    digitHour1++;
+  }
+  if (digitHour1 === 10) {
+    digitHour1 = 0;
+    digitHour2++;
+  }
+  if (digitHour2 === 2 && digitHour1 === 4) {
+    digitHour2 = 0;
+    digitHour1 = 0;
+  }
+
+    ReactDOM.render( <Home
+      segundo1={digitSec1}
+      segundo2={digitSec2}
+      minuto1={digitMin1}
+      minuto2={digitMin2}
+      hora1={digitHour1}
+      hora2={digitHour2}
+    /> , document.querySelector("#app"));
+}, 1000)
